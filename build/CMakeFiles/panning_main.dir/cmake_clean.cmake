230feaacc416a2f32143acd5a7f6f913516e80cb
xiaoyu <wangxiaoyu_98@outlook.com>
2024-04-20 16:13:28 +0800

    version 0.0.1

---
+++ build/CMakeFiles/panning_main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/WANGX/Desktop/CMake_test/cmake_planning_test/bin/libpanning_main.dll.a"
+  "C:/Users/WANGX/Desktop/CMake_test/cmake_planning_test/bin/panning_main.exe"
+  "C:/Users/WANGX/Desktop/CMake_test/cmake_planning_test/bin/panning_main.exe.manifest"
+  "C:/Users/WANGX/Desktop/CMake_test/cmake_planning_test/bin/panning_main.pdb"
+  "CMakeFiles/panning_main.dir/planning_main.cpp.obj"
+  "CMakeFiles/panning_main.dir/planning_main.cpp.obj.d"
+  "CMakeFiles/panning_main.dir/pnc_map.cpp.obj"
+  "CMakeFiles/panning_main.dir/pnc_map.cpp.obj.d"
+  "CMakeFiles/panning_main.dir/process.cpp.obj"
+  "CMakeFiles/panning_main.dir/process.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/panning_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/panning_main.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for panning_main.
+# This may be replaced when dependencies are built.
