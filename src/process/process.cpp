b88964efa14b8511ad4326cb69802bcb02f425b4
xiaoyu <wangxiaoyu_98@outlook.com>
2024-04-21 17:25:39 +0800

    add dynastic third party eigen3

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 #include "process.h"
+#include <Eigen/Dense>
 using std::cout,std::endl;
+using Eigen::MatrixXd;
 
 void Process::planProcess(){
     cout<<"this is planning process"<<endl;
     my_map.mapInfo();
-    cout<<"planning success!"<<endl;
+
+    MatrixXd m(2,2);
+    m(0,0)=3;
+    m(0,1)=-1;
+    m(1,0)=6;
+    m(1,1)=m(1,0)+m(0,1);
+    cout<<m<<endl;
+    
+    cout<<"panning process success"<<endl;
 }
